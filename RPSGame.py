e0153af69ddc6f3b977504ebba169e10a1520377
Le <t_le86524@tss-hr420-dm23.fc.ca>
2018-10-25 15:45:07 -0400

    RPS game new rules

--- RPSGame.py
+++ RPSGame.py
@@ -2,9 +2,56 @@ from random import randint
 
 #avaikable weapons => Store this is an array
 choices = ["Rock","Paper","Scissors"]
+player = False
 
 # Make the computer pick one item at random
-computer_choice = choices[randint(0, 2)]
+computer = choices[randint(0, 2)]
 
 #Show the computer's choice in the terminal window
-print("Computer chooses: ",computer_choice)
+print("Computer chooses: ",computer)
+
+while player is False:
+	player = input("Rock,Paper or Scissors? \n")
+	print("Player chooses:", player)
+
+if(player== computer):
+	print("Tie!Live to shoot another day")
+
+elif player == "Rock":
+		if computer == "Paper":
+			# computer won
+			print("You lose", computer,"covers", player)
+		else:
+			print("You win",player,"smashes",computer)
+
+elif player=="Paper":
+		if computer == "Scissor":
+			print("You lose", computer,"cuts", player)
+		else:
+			print("You win",player,"covers",computer)
+
+elif player=="Scissor":
+		if computer == "Rock":
+			print("You lose", computer,"smashes", player)
+		else:
+			print("You win",player,"cuts",computer)
+
+elif player == "Quit":
+	exit()
+
+else:
+	print("Not a valid option. Check again and check your spelling!\n")
+
+	player == False
+	computer = choices[randint(0, 2)]
+
+
+
+
+
+
+
+
+
+
+
