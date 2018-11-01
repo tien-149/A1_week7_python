5fb6ca2536acde0576438dff73b3c66203db3308
Le <t_le86524@tss-hr420-dm12.fc.ca>
2018-11-01 14:27:51 -0400

    add lives

--- RPSGame.py
+++ RPSGame.py
@@ -1,5 +1,8 @@
 from random import randint
 
+player_lives = 5
+computer_lives = 5
+
 #avaikable weapons => Store this is an array
 choices = ["Rock","Paper","Scissors"]
 player = False
@@ -11,6 +14,8 @@ computer = choices[randint(0, 2)]
 print("Computer chooses: ",computer)
 
 while player is False:
+	print("====================================")
+	print("====================================")
 	player = input("Rock,Paper or Scissors? \n")
 	print("Player chooses:", player)
 
@@ -20,21 +25,27 @@ if(player== computer):
 elif player == "Rock":
 		if computer == "Paper":
 			# computer won
+			player_lives -= 1
 			print("You lose", computer,"covers", player)
 		else:
 			print("You win",player,"smashes",computer)
+			computer_lives -= 1
 
 elif player=="Paper":
 		if computer == "Scissor":
+			player_lives -= 1
 			print("You lose", computer,"cuts", player)
 		else:
 			print("You win",player,"covers",computer)
+			computer_lives -= 1
 
 elif player=="Scissor":
 		if computer == "Rock":
+			player_lives -= 1
 			print("You lose", computer,"smashes", player)
 		else:
 			print("You win",player,"cuts",computer)
+			computer_lives -= 1
 
 elif player == "Quit":
 	exit()
